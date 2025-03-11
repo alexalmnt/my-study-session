8f9707564f378da3dc1d6273520f6e1ffa455a4c
Alex <166076626+alexalmnt@users.noreply.github.com>
2025-03-10 20:48:48 -0400

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-
-  required_version = ">= 0.14"  # Or specify your required version
-}
 
 provider "aws" {
   region = "us-east-1"
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,12 @@
 provider"aws"{
     region = "us-east-1"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"  # Adjust the version to your preference
+    }
+  }
+}
