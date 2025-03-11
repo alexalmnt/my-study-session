c286a9a7c4a43450af894be2a17bde9876e0acb1
Alex <166076626+alexalmnt@users.noreply.github.com>
2025-03-10 20:35:48 -0400

    fixed terra code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-
-  backend "s3" {
-    region = "us-east-1"
-    key    = "terraform.tfstate"
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_instance" "test_instance" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
