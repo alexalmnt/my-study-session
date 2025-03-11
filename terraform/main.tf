819b9c8999e8398a42526fb14c68d0757a35278f
Alex <166076626+alexalmnt@users.noreply.github.com>
2025-03-10 20:46:34 -0400

    almost there

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 0.14"  # Or specify your required version
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_instance" "test_instance" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
